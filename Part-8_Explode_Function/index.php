7727a33f9461adfc1e812335edf37a261479f1d6
Karthi Fair Hawn <karthifairhawn@gmail.com>
2021-03-28 15:27:31 +0530

    Saving to DB

--- Part-8_Explode_Function/index.php
+++ Part-8_Explode_Function/index.php
@@ -30,8 +30,14 @@
 				<div class="col s6 md3">
 					<div class="card z-depth-0">
 						<div class="card-content center">
-							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-							<div><?php echo htmlspecialchars($pizza['ingrediants']); ?></div>
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>							
+								<ul class="grey-text">
+									<?php foreach(explode(',',$pizza['ingrediants']) as $ing) {?>								
+										<li> 
+											<?php echo htmlspecialchars($ing)?>
+										</li>
+									<?php }?>
+								</ul>
 						</div>
 						<div class="card-action right-align">
 							<a class="brand-text" href="#">more info</a>
---
+++ Part-9_Saving_to_Database/config/db_connect.php
@@ -0,0 +1,9 @@
+<?php 
+	$conn = mysqli_connect('localhost','karthi','karthi12','ninja_pizza');
+	if (!$conn){
+		echo 'Connection Error to DB';
+	}
+		
+	
+
+            ?>            
